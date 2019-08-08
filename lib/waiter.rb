64a1c9f3cd8ab7d1910867a189dc793f9665640a
PollyCR <polly.c.reynolds@gmail.com>
2019-08-08 17:27:42 +0100

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
+attr_reader :name, :experience
 
+@@all = []
+def initialize(name, experience)
+    @name = name 
+    @experience = experience 
+    @@all << self
+end 
+
+def self.all
+    return @@all
 end
+
+def new_meal(waiter=self,customer,total,tip)
+    meal = Meal.new(waiter,customer,total,tip)
+end 
+
+def meals 
+    Meal.all.select {|meal|meal.waiter == self}
+end
+
+def best_tipper 
+  best_tipped_meal = Meal.all.max_by {|meal|meal.tip}
+  best_tipped_meal.customer
+end 
+end 
